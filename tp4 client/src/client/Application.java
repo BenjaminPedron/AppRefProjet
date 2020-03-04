3a4521cac7960adfdce91be534d8e1dd927c0ec5
benjaminGUYT <55363637+benjaminGUYT@users.noreply.github.com>
2020-03-04 11:41:18 +0100

    Update Application.java

--- tp4 client/src/client/Application.java
+++ tp4 client/src/client/Application.java
@@ -6,50 +6,50 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-/*
- * Ce client se connecte à un serveur dont le protocole est 
- * menu-choix-question-réponse client-réponse service
- * il n'y a qu'un échange (pas de boucle)
- * la réponse est saisie au clavier en String
- * 
- * Le protocole d'échange est suffisant pour le tp4 avec ServiceInversion 
- * ainsi que tout service qui pose une question, traite la donnée du client et envoie sa réponse 
- * mais est bien sur susceptible de (nombreuses) améliorations
- */
+/**
+ * Le client se connecte Ã  un serveur dont le protocole est 
+ * menu-choix-question-rÃ©ponse client-rÃ©ponse service
+ * la rÃ©ponse est saisie au clavier en String
+ **/
 class Application {
+	
 		private final static int PORT_SERVICE = 3000;
-		private final static String HOST = "localhost"; 
+		private final static String HOST = "localhost";
 	
 	public static void main(String[] args) {
 		Socket s = null;		
 		try {
+			/* On crÃ©Ã© la socket allant questionner le serveur */
 			s = new Socket(HOST, PORT_SERVICE);
 
 			BufferedReader sin = new BufferedReader (new InputStreamReader(s.getInputStream ( )));
 			PrintWriter sout = new PrintWriter (s.getOutputStream ( ), true);
 			BufferedReader clavier = new BufferedReader(new InputStreamReader(System.in));			
-		
-			System.out.println("Connecté au serveur " + s.getInetAddress() + ":"+ s.getPort());
+			
+			System.out.println("ConnectÃ© au serveur " + s.getInetAddress() + ":"+ s.getPort());
 			
 			String line;
-		// menu et choix du service
+			/* Menu et choix du service */
 			line = sin.readLine();
 			System.out.println(line.replaceAll("##", "\n"));
-		// saisie/envoie du choix
+			/* Saisie/envoie du choix */
 			sout.println(clavier.readLine());
 			
-		// réception/affichage de la question
+			/* RÃ©ception/affichage de la question */
 			System.out.println(sin.readLine());
-		// saisie clavier/envoie au service de la réponse
+			/* Saisie clavier/envoie au service de la rÃ©ponse */
 			sout.println(clavier.readLine());
-		// réception/affichage de la réponse
+			/* RÃ©ception/affichage de la rÃ©ponse */
 			System.out.println(sin.readLine());
 				
 			
 		}
-		catch (IOException e) { System.err.println("Fin de la connexion"); }
-		// Refermer dans tous les cas la socket
-		try { if (s != null) s.close(); } 
-		catch (IOException e2) { ; }		
+		catch (IOException e) { 
+			System.err.println("Fin de la connexion"); 
+		}
+		/* Refermer dans tous les cas la socket */
+		try { 
+			if (s != null) s.close(); 
+		} catch (IOException e2) {}		
 	}
 }
